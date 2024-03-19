fd18807115975037d3d416e51cb55f532d42779a
Dimitar Iliev <diki_tazo@hotmail.com>
2024-03-19 23:41:08 +0100

    Add project files.

---
+++ SearchMultiSrcIndex/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Search.Documents.Indexes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchMultiSrcIndex.Models
+{
+    public class Product
+    {
+        [JsonProperty(PropertyName = "productId")]
+        [SimpleField(IsFilterable = true, IsKey = true)]
+        public string ProductId { get; set; }
+
+        [JsonProperty(PropertyName = "productName")]
+        [SimpleField(IsFilterable = true)]
+        public string ProductName { get; set; }
+
+        [JsonProperty(PropertyName = "price")]
+        [SimpleField(IsFilterable = true)]
+        public int Price { get; set; }
+
+        // Data from other data source
+        [JsonProperty(PropertyName = "description")]
+        [SimpleField(IsFilterable = true)]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "available")]
+        [SimpleField(IsFilterable = true)]
+        public bool Available { get; set; }
+
+        [JsonProperty(PropertyName = "shopLocation")]
+        [SimpleField(IsFilterable = true)]
+        public string ShopLocation { get; set; }
+    }
+}
---
+++ SearchMultiSrcIndex/Program.cs
@@ -0,0 +1,133 @@
+﻿using Azure;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
+using Microsoft.Extensions.Configuration;
+using SearchMultiSrcIndex.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AzureSearch.SDKHowTo
+{
+    public sealed class Program
+    {
+        static IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+        static IConfigurationRoot configuration = builder.Build();
+
+        static async Task Main(string[] args)
+        {
+            var searchServiceUri = configuration["SearchServiceUri"];
+            var adminApiKey = configuration["SearchServiceAdminApiKey"];
+
+            var indexClient = new SearchIndexClient(new Uri(searchServiceUri), new AzureKeyCredential(adminApiKey));
+            var indexerClient = new SearchIndexerClient(new Uri(searchServiceUri), new AzureKeyCredential(adminApiKey));
+
+            var indexName = "products-index";
+
+            await CreateIndexAsync(indexName, indexClient);
+
+            await CreateAndRunCosmosDbIndexerAsync(indexName, indexerClient);
+
+            await CreateAndRunBlobIndexerAsync(indexName, indexerClient);
+
+            Console.WriteLine("Index created.  Press any key to end application...\n");
+            Console.ReadKey();
+        }
+
+        private static async Task CreateIndexAsync(string indexName, SearchIndexClient indexClient)
+        {
+            var bulder = new FieldBuilder();
+            var definition = new SearchIndex(indexName, bulder.Build(typeof(Product)));
+
+            await indexClient.CreateIndexAsync(definition);
+        }
+
+        private static async Task CreateAndRunCosmosDbIndexerAsync(string indexName, SearchIndexerClient indexerClient)
+        {
+            var cosmosConnectString = $"{configuration["CosmosDBConnectionString"]};Database={configuration["CosmosDBDatabaseName"]}";
+
+            var cosmosDbDataSource = new SearchIndexerDataSourceConnection(
+                name: configuration["CosmosDBDatabaseName"],
+                type: SearchIndexerDataSourceType.CosmosDb,
+                connectionString: cosmosConnectString,
+                container: new SearchIndexerDataContainer("Products"));
+          
+            await indexerClient.CreateOrUpdateDataSourceConnectionAsync(cosmosDbDataSource);
+
+            Console.WriteLine("Creating Cosmos DB indexer...\n");
+
+            var cosmosDbIndexer = new SearchIndexer(
+                name: "products-indexer",
+                dataSourceName: cosmosDbDataSource.Name,
+                targetIndexName: indexName)
+            {
+                Schedule = new IndexingSchedule(TimeSpan.FromDays(1))
+            };
+
+            try
+            {
+                await indexerClient.GetIndexerAsync(cosmosDbIndexer.Name);
+
+                await indexerClient.ResetIndexerAsync(cosmosDbIndexer.Name);
+            }
+            catch (RequestFailedException ex) when (ex.Status == 404) { }
+
+            await indexerClient.CreateOrUpdateIndexerAsync(cosmosDbIndexer);
+
+            Console.WriteLine("Running Cosmos DB indexer...\n");
+
+            try
+            {
+                await indexerClient.RunIndexerAsync(cosmosDbIndexer.Name);
+            }
+            catch (RequestFailedException ex) when (ex.Status == 429)
+            {
+                Console.WriteLine("Failed to run indexer: {0}", ex.Message);
+            }
+        }
+
+        private static async Task CreateAndRunBlobIndexerAsync(string indexName, SearchIndexerClient indexerClient)
+        {
+            var blobDataSource = new SearchIndexerDataSourceConnection(
+                name: configuration["BlobStorageAccountName"],
+                type: SearchIndexerDataSourceType.AzureBlob,
+                connectionString: configuration["BlobStorageConnectionString"],
+                container: new SearchIndexerDataContainer("productdata"));
+
+            await indexerClient.CreateOrUpdateDataSourceConnectionAsync(blobDataSource);
+
+            var parameters = new IndexingParameters();
+            parameters.Configuration.Add("parsingMode", "json");
+
+            var blobIndexer = new SearchIndexer(
+                name: "products-blob-indexer",
+                dataSourceName: blobDataSource.Name,
+                targetIndexName: indexName)
+            {
+                Parameters = parameters,
+                Schedule = new IndexingSchedule(TimeSpan.FromDays(1))
+            };
+
+            try
+            {
+                await indexerClient.GetIndexerAsync(blobIndexer.Name);
+
+                await indexerClient.ResetIndexerAsync(blobIndexer.Name);
+            }
+            catch (RequestFailedException ex) when (ex.Status == 404) { }
+
+            await indexerClient.CreateOrUpdateIndexerAsync(blobIndexer);
+
+            Console.WriteLine("Running Blob Storage indexer...\n");
+
+            try
+            {
+                await indexerClient.RunIndexerAsync(blobIndexer.Name);
+            }
+            catch (RequestFailedException ex) when (ex.Status == 429)
+            {
+                Console.WriteLine("Failed to run indexer: {0}", ex.Message);
+            }
+        }
+    }
+}
