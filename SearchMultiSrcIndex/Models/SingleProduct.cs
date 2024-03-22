a20060c60e73b970922f35422759622a8e31ce3e
Dimitar Iliev <diki_tazo@hotmail.com>
2024-03-22 23:31:23 +0100

    Change detection policy and data deletion detection policy example.

---
+++ SearchMultiSrcIndex/Models/SingleProduct.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Search.Documents.Indexes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchMultiSrcIndex.Models
+{
+    public class SingleProduct
+    {
+        [JsonProperty(PropertyName = "productId")]
+        [SimpleField(IsFilterable = true, IsKey = true)]
+        public string ProductId { get; set; }
+
+        [JsonProperty(PropertyName = "productName")]
+        [SimpleField(IsFilterable = true)]
+        public string ProductName { get; set; }
+
+        [JsonProperty(PropertyName = "price")]
+        [SimpleField(IsFilterable = true)]
+        public int Price { get; set; }
+    }
+}
--- SearchMultiSrcIndex/Program.cs
+++ SearchMultiSrcIndex/Program.cs
@@ -16,22 +16,61 @@ namespace AzureSearch.SDKHowTo
 
         static async Task Main(string[] args)
         {
+            var runMultiSource = false;
+
             var searchServiceUri = configuration["SearchServiceUri"];
             var adminApiKey = configuration["SearchServiceAdminApiKey"];
 
             var indexClient = new SearchIndexClient(new Uri(searchServiceUri), new AzureKeyCredential(adminApiKey));
             var indexerClient = new SearchIndexerClient(new Uri(searchServiceUri), new AzureKeyCredential(adminApiKey));
 
+            if (runMultiSource)
+            {
+                await MultiSourceIndex(indexClient, indexerClient);
+            }
+            else
+            {
+                await SingleSourceIndex(indexClient, indexerClient);
+            }
+            
+
+
+        }
+
+        private static async Task SingleSourceIndex(SearchIndexClient indexClient, SearchIndexerClient indexerClient)
+        {
+            // use DataChangeDetectionPolicy and dataDeletionDetectionPolicy
+
+            var indexName = "single-product-index";
+
+            await CreateSingleIndexAsync(indexName, indexClient);
+
+            await CreateAndRunCosmosDbSingleIndexerAsync(indexName, indexerClient);
+
+            Console.WriteLine("Index created.  Press any key to end application...\n");
+            Console.ReadKey();
+        }
+
+        private static async Task MultiSourceIndex(SearchIndexClient indexClient, SearchIndexerClient indexerClient)
+        {
             var indexName = "products-index";
 
             await CreateIndexAsync(indexName, indexClient);
 
-            await CreateAndRunCosmosDbIndexerAsync(indexName, indexerClient);
+            await CreateAndRunCosmosDbSingleIndexerAsync(indexName, indexerClient);
 
             await CreateAndRunBlobIndexerAsync(indexName, indexerClient);
 
             Console.WriteLine("Index created.  Press any key to end application...\n");
             Console.ReadKey();
+        }
+
+        private static async Task CreateSingleIndexAsync(string indexName, SearchIndexClient indexClient)
+        {
+            var bulder = new FieldBuilder();
+            var definition = new SearchIndex(indexName, bulder.Build(typeof(SingleProduct)));
+
+            await indexClient.CreateIndexAsync(definition);
         }
 
         private static async Task CreateIndexAsync(string indexName, SearchIndexClient indexClient)
@@ -40,6 +79,58 @@ namespace AzureSearch.SDKHowTo
             var definition = new SearchIndex(indexName, bulder.Build(typeof(Product)));
 
             await indexClient.CreateIndexAsync(definition);
+        }
+
+        private static async Task CreateAndRunCosmosDbSingleIndexerAsync(string indexName, SearchIndexerClient indexerClient)
+        {
+            var cosmosConnectString = $"{configuration["CosmosDBConnectionString"]};Database={configuration["CosmosDBDatabaseName"]}";
+
+            var cosmosDbDataSource = new SearchIndexerDataSourceConnection(
+                name: configuration["CosmosDBDatabaseName"],
+                type: SearchIndexerDataSourceType.CosmosDb,
+                connectionString: cosmosConnectString,
+                container: new SearchIndexerDataContainer("Products"));
+
+            cosmosDbDataSource.DataChangeDetectionPolicy = new HighWaterMarkChangeDetectionPolicy("_ts");
+
+            cosmosDbDataSource.DataDeletionDetectionPolicy = new SoftDeleteColumnDeletionDetectionPolicy
+            {
+                SoftDeleteColumnName = "isDeleted",
+                SoftDeleteMarkerValue = "true"
+            };
+
+            await indexerClient.CreateOrUpdateDataSourceConnectionAsync(cosmosDbDataSource);
+
+            Console.WriteLine("Creating Cosmos DB indexer...\n");
+
+            var cosmosDbIndexer = new SearchIndexer(
+                name: "single-product-indexer",
+                dataSourceName: cosmosDbDataSource.Name,
+                targetIndexName: indexName)
+            {
+                Schedule = new IndexingSchedule(TimeSpan.FromDays(1))
+            };
+
+            try
+            {
+                await indexerClient.GetIndexerAsync(cosmosDbIndexer.Name);
+
+                await indexerClient.ResetIndexerAsync(cosmosDbIndexer.Name);
+            }
+            catch (RequestFailedException ex) when (ex.Status == 404) { }
+
+            await indexerClient.CreateOrUpdateIndexerAsync(cosmosDbIndexer);
+
+            Console.WriteLine("Running Cosmos DB indexer...\n");
+
+            try
+            {
+                await indexerClient.RunIndexerAsync(cosmosDbIndexer.Name);
+            }
+            catch (RequestFailedException ex) when (ex.Status == 429)
+            {
+                Console.WriteLine("Failed to run indexer: {0}", ex.Message);
+            }
         }
 
         private static async Task CreateAndRunCosmosDbIndexerAsync(string indexName, SearchIndexerClient indexerClient)
@@ -51,7 +142,7 @@ namespace AzureSearch.SDKHowTo
                 type: SearchIndexerDataSourceType.CosmosDb,
                 connectionString: cosmosConnectString,
                 container: new SearchIndexerDataContainer("Products"));
-          
+
             await indexerClient.CreateOrUpdateDataSourceConnectionAsync(cosmosDbDataSource);
 
             Console.WriteLine("Creating Cosmos DB indexer...\n");
